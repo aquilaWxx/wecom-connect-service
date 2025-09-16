ab12df29741d0c71050a49e1a74c5c3a1ad898ad
aquila <aquilawxx@qq.com>
2025-09-16 20:02:38 +0800

    refactor(weixin): 重构企业微信登录流程
    - 移除了 ApiController 中的 getWecomLoginParams 方法
    - 更新了 wecom-login.html 中的登录逻辑，直接使用 URL 参数
    - 简化了企业微信登录的流程，提高了代码的可读性和维护性

--- src/main/java/tech/zenblob/wecomconnectservice/controller/apis/ApiController.java
+++ src/main/java/tech/zenblob/wecomconnectservice/controller/apis/ApiController.java
@@ -1,42 +1,17 @@
 package tech.zenblob.wecomconnectservice.controller.apis;
 
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tech.zenblob.wecomconnectservice.controller.service.weixin.WecomService;
-import tech.zenblob.wecomconnectservice.pojo.BaseResponse;
-import tech.zenblob.wecomconnectservice.pojo.WecomParam;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    @Autowired
-    private WecomService wecomService;
-
     @GetMapping("/data")
     public String getData() {
         return "这是 Spring Boot API 返回的数据";
-    }
-
-
-    /**
-     * 获取企业微信登录所需的相关参数
-     */
-    @GetMapping(value = "/loginParams")
-    @ApiOperation(value = "获取企业微信登录所需的相关参数")
-    public BaseResponse<WecomParam> getWecomLoginParams(String corpId) {
-        try {
-            WecomParam wecomParam = wecomService.getLoginParams(corpId);
-            return BaseResponse.success(wecomParam);
-        } catch (Exception e) {
-            log.error("获取企业微信登录参数失败");
-            e.printStackTrace();
-            return BaseResponse.failed("获取企业微信登录参数失败");
-        }
     }
 }
